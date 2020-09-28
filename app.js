7c2135643ffd9a027fef15fc2bb8cfcbb2daf82d
Gastón Dominguez <gastonmd14@gmail.com>
2020-09-28 12:43:33 -0300

    modifique app,js agregue nueva ruta a productDtail

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // http://localhost:3000/
 // http://mitienda.com/
 
-const express = require (`express`);
+const express = require ('express');
 const app = express ();
 const path = require("path");
+
+app.listen(3000, () => console.log('Server running on port 3000'));
 
 
 app.get ("/", function(req,res){
@@ -25,5 +27,3 @@ app.get("*", function (req,res){
     
     res.send("Not found")
 })
-
-app.listen(3000)
