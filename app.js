9ff46c4d23ebe88a12e8987e28a3aa9d33b978d7
rocioromeroo <rocio.aldanaromero@gmail.com>
2020-09-28 19:26:00 -0300

    Header y Footer

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.get ('/', function(req, res) {
     res.sendFile(file);
 });
 
+app.get ('/header', function(req, res) {
+    let file = path.resolve('views/header-footer.html');
+    res.sendFile(file);
+});
+
 app.get('/productDetail', function(req, res) {
     let file = path.resolve('views/productDetail.html');
     res.sendFile(file);
