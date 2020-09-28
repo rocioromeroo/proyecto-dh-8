cc60c1a7474325f8ee51b5ac29517034d449d84c
Gastón Dominguez <gastonmd14@gmail.com>
2020-09-28 13:31:27 -0300

    nueva modificaion app.js

--- app.js
+++ app.js
@@ -8,22 +8,20 @@ const path = require("path");
 app.listen(3000, () => console.log('Server running on port 3000'));
 
 
-app.get ("/", function(req,res){
+app.get ("/", function(req,res) {
     let file = path.resolve ("views/index.html")
     res.sendFile(file)
 })
 
-app.get("/productDetail", function(req,res){
+app.get("/productDetail", function(req, res) {
     let file = path.resolve ("views/productDetail.html")
     res.sendFile(file)
 })
 
-app.get("*", function (req,res){
+app.get("*", function (req, res) {
     if (req.url.includes(".")){
         let file= path.resolve ("public" + req.url)
         return res.sendFile(file)
-    }
-
-    
+    }    
     res.send("Not found")
 })
