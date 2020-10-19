490be0cfd8d8d6f42779a56633ce27a036528d79
lnusfaumer <lnusfaumer@gmail.com>
2020-10-19 15:37:34 +1000

    agregue data y un json, y algunos productos mas con sus fotos

--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,7 @@ let productsController= require("../controllers/productsController");
 /* GET Products page. */
 router.get('/', productsController.products)
 
-/* GET Products/accesorios page. */
+/* GET Products/acc page. */
 router.get('/acc', productsController.accessories)
 
 
