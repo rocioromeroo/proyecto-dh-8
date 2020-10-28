53c00802179841c1e4a7ec6288295753173e87b1
Gastón Dominguez <gastonmd14@gmail.com>
2020-10-28 18:43:24 -0300

    se modifican etiquetas de css que generan problemas con el header

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,45 +2,49 @@ var path = require('path');
 var fs = require('fs');
 const productsList = require('../data/productsDataBase');
 const { PreconditionFailed } = require('http-errors');
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const productsController= {
 
-      products: function(req, res){
-          res.render("./product/product")
-      },
+  products: function(req, res){
 
-      accessories: function(req, res){
-        let items = productsList.filter(function(valor){
-          if (valor.category == "accesorios"){
-            return valor
-          }
-        })
-        
-        res.render('./product/accessories', {items: items})
-    },
+      res.render("./product/product")
+  },
+
+  accessories: function(req, res){
+    let items = productsList.filter(function(valor){
+      if (valor.category == "accesorios"){
+        return valor
+      }
+    })
+    
+    res.render('./product/accessories', {items: items})
+  },
      
-    detail: function(req, res){
-      let detalle = productsList.filter(function(valor){
-        if (valor.id == req.params.id){
-          return valor
-        }
-      })
+  detail: function(req, res){
+    let detalle = productsList.filter(function(valor){
+      if (valor.id == req.params.id){
+        return valor
+      }
+    })
 
-      let verificar = productsList.filter(function(dato){
-        if (dato.category == "accesorios"){
-          return dato
-        }
-      })
+    let verificar = productsList.filter(function(dato){
+      if (dato.category == "accesorios"){
+        return dato
+      }
+    })
 
-      let detalleAccesorio = verificar.slice(3, 6);
+    let detalleAccesorio = verificar.slice(3, 6);
 
-      
 
-      res.render("./product/productDetail", {detalle: detalle, detalleAccesorio: detalleAccesorio})
+    
+ 
+    res.render("./product/productDetail", {detalle: detalle, detalleAccesorio: detalleAccesorio})
   },
     
-  create: function(req, res, next){
-    res.render("product/createProduct")
+create: function(req, res, next){
+
+  res.render("product/createProduct")
 },
 
 store: function (req, res, next) {
