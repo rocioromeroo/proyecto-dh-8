3ce0f9507da39dac5a61bdc50a9f92b8259c578d
rocioromeroo <rocio.aldanaromero@gmail.com>
2020-11-25 20:37:13 -0300

    Actualización

---
+++ middleware/rememberMiddleware.js
@@ -0,0 +1,8 @@
+var userData = require('../models/user')
+module.exports = function (req, res, next) {
+    if(req.session.recordame && !req.session.user) {
+        let user = userData.findByEmail(req.cookie.recordame)
+        req.session.user = user.email
+    }
+    next()
+}
