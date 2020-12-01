d132460f0d99dafd6caf74bbd9ea1aac4d4ac23f
rocioromeroo <rocio.aldanaromero@gmail.com>
2020-12-01 20:01:27 -0300

    Sprint-5

--- middleware/userValidator.js
+++ middleware/userValidator.js
@@ -1,35 +1,3 @@
-// const modelsUsers = require("../models/user")
-// const {check, body} = require('express-validator')
-// const fs = require('fs')
-
-
-// module.exports = [
-//       check('name').isLength({min:1}).withMessage('Campo incompleto'),
-
-//       check('surname').isLength({min:1}).withMessage('Campo incompleto'),
-
-//       check('email')
-//       .isEmail().withMessage('No es un email valido'),
-
-//       check('password')
-//       .isLength({min:8}).withMessage('Su clave debe tener almenos 8 caracteres'),
-//       check('password')
-//       .isAlphanumeric().withMessage('Su clave debe contener numeros y letras'),
-
-//       // check('passwordRepeat')
-//       // .isLength({min:8}).withMessage('Campo incompleto')
-//       // .isAlphanumeric().withMessage('Repetir clave'),
-
-//       body('email').custom(function(value){
-//            let user = modelsUsers.findByEmail(value)
-//            if (user){
-//             throw new Error('Este email ya fue utilizado');
-//            }
-//            else{ return true}
-//       })
-
-// ]
-
 const { body, check } = require("express-validator");
 let modelsUsers = require("../models/user");
 
@@ -51,44 +19,4 @@ module.exports = [
 
   check('password').isLength({min:4}).withMessage('Su clave debe tener almenos 4 caracteres')
 ];
-<<<<<<< HEAD
-=======
-      check('password')
-<<<<<<< HEAD
-<<<<<<< HEAD
-      .isLength({min:8}).withMessage('Su clave debe tener almenos 8 caracteres'),
-      check('password')
-      .isAlphanumeric().withMessage('Su clave debe contener numeros y letras'),
 
-      // check('passwordRepeat')
-      // .isLength({min:8}).withMessage('Campo incompleto')
-      // .isAlphanumeric().withMessage('Repetir clave'),
-
-      body('email').custom(function(value){
-           let user = modelsUsers.findByEmail(value)
-           if (user){
-            throw new Error('Este email ya fue utilizado');
-           }
-           else{ return true}
-      })
-
-]
-=======
-      .isLength({min:2}).withMessage('Su clave debe tener almenos 8 caracteres'),
-=======
-      .isLength({min:6}).withMessage('Su clave debe tener almenos 6 caracteres'),
-
-      body("email").custom(function(value){
-            let user = modelsUsers.findByEmail(value)
-
-            if(user) {
-                  throw new Error ('Este email ya se encuentra registrado')
-            }
-            return true
-      })
->>>>>>> aa261ea914730995d9696d7fcc4b19a01dc7c650
-     
-]
->>>>>>> 22134f189646480c499a98a5bc620bd76cfed318
-=======
->>>>>>> 64c6b275a25e395caffcd0c31b13960de07e9f00
