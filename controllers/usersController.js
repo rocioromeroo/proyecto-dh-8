61765324af336bd5159bb7e16ad0fcdca806139c
lnusfaumer <lnusfaumer@gmail.com>
2020-12-03 09:25:15 +1000

    my account vista

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,119 +1,31 @@
-var path = require('path');
-var fs = require('fs');
-const { validationResult } = require("express-validator")
-const usersList = require('../data/usersDataBase')
-const productsList = require('../data/productsDataBase');
-const bcryptjs = require("bcryptjs")
-var modelsUsers = require("../models/user")
-
-module.exports = {
-      myAccount: function (req, res) {
-            let dato = productsList.find(function (valor) {
-                  if (valor.id == productsList.length) {
-                    return valor
-                  }
-            })
-            res.render("user/myAccount", { styleOn: "style", dato:dato})
-          },
-      editPerfil:function (req, res) {
-                
-            res.render("user/editPerfil", { styleOn: "register"})
-          },
-
-      register: function(req, res){
-        res.render('user/register',{styleOn: "register", errors:{}})
-      },
-
-      login:function(req, res){
-            let errors = {}
-            res.render("user/login",{errors, styleOn: "login"} )
-      },   
-
-      processlogin:function(req, res){
-            let errors = validationResult(req)
-            let user = modelsUsers.findByEmail(req.body.email)
-
-            if(!user) {
-              return res.render('user/login', {errors: errors.mapped(), styleOn: "login"})
-
-            } else if (bcryptjs.compareSync(req.body.password, user.password)) {
-                req.session.user = user.email     
-
-               if(req.body.recordame) {
-                 res.cookie('recordame', user.email, {maxAge: 120 * 1000})                                                        
-               }
-              return res.render('user/myAccount', {styleOn: "style"})
-        
-              } else {
-                  return res.render('user/login', {errors: errors.mapped(), styleOn: "login"})
-              }
-          },        
-         
-      logout:function(req, res){
-            res.render("user/login",{styleOn: "login"} )
-      },   
-
-      contact:function(req, res){
-            res.render("user/contact", {styleOn: "contact"})
-      },
-
-      comment:function(req, res){
-
-            let pathFile = path.join('data','comment.json')
-
-            let nuevoMessage = fs.readFileSync(pathFile, { encoding: 'utf-8' })
-        
-            nuevoMessage = JSON.parse(nuevoMessage)
-        
-            nuevoMessage.push({
-              ...req.body,
-              id: nuevoMessage[nuevoMessage.length - 1].id + 1,
-            })
-        
-            nuevoMessage = JSON.stringify(nuevoMessage)
-        
-            fs.writeFileSync(pathFile, nuevoMessage)
-            
-            res.send('Mensaje Recibido!!')
-      },
-
-      userStore: function(req, res){
-            let errors = validationResult(req)
-
-            if(errors.isEmpty()) {
-                  modelsUsers.create({    
-                        email :req.body.email,
-                        password: bcryptjs.hashSync(req.body.password),
-                        surname:req.body.surname,
-                  }) 
-
-            res.render("user/myAccount", {styleOn:"style"})
-            }
-            else{
-
-                 return res.render('user/register', {
-                        errors: errors.mapped(),
-                        styleOn:"register"
-                  })
-           
-             }
-     }
-}
-
-    
-      
-
-      
-
-  
 
 var path = require("path");
 var fs = require("fs");
+const usersList = require('../data/usersDataBase')
 const { validationResult } = require("express-validator");
 const bcryptjs = require("bcryptjs");
 var modelsUsers = require("../models/user");
 
 module.exports = {
+  myAccount: function (req, res) {
+    let dato = usersList.find(function (valor) {
+          if (valor.id == usersList.length) {
+            return valor
+          }
+    })
+    res.render("user/myAccount", { styleOn: "style", dato:dato})
+  },
+  
+editPerfil:function (req, res) {
+        
+    res.render("user/editPerfil", { styleOn: "register"})
+  },
+
+  savePerfil:function (req, res) {
+        
+    res.render("user/editPerfil", { styleOn: "register"})
+  },
+
   login: function (req, res) {
     let errors = {};
     res.render("user/login", { errors, styleOn: "login" });
--- routes/users.js
+++ routes/users.js
@@ -4,16 +4,28 @@ var router = express.Router();
 const usersController = require('../controllers/usersController')
 const userValidator = require("../middleware/userValidator")
 const userLoginValidator = require("../middleware/userLoginValidator")
+var multer  = require('multer');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
+  }
+})
+var upload = multer({ storage: storage });
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 /*        GET       MyAccount   */
-router.get('/account', usersController.myAccount)
+router.get('/account/', usersController.myAccount)
 
 /*        GET       EDITAR PERFIL   */
 router.get('/editPerfil', usersController.editPerfil)
+router.put('/editPerfil',upload.any(), usersController.savePerfil)
 
 
 /*        GET/POST       Register   */
