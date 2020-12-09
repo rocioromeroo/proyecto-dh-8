8f4a292c26f193cd58c4fdccffd233b3503cd576
Gastón Dominguez <gastonmd14@gmail.com>
2020-12-09 17:29:11 -0300

    se deja funcionando el metodo de actualizar producto

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,210 +1,208 @@
-var path = require('path');
-var fs = require('fs');
-const productsList = require('../data/productsDataBase');
-const guaranteeList = require('../data/guaranteeDataBase');
-const featuresList = require('../data/featuresDataBase');
-const { PreconditionFailed } = require('http-errors');
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-const usersList = require('../data/usersDataBase')
-
+var path = require("path");
+var fs = require("fs");
+const productsList = require("../data/productsDataBase");
+const guaranteeList = require("../data/guaranteeDataBase");
+const featuresList = require("../data/featuresDataBase");
+const { PreconditionFailed } = require("http-errors");
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const usersList = require("../data/usersDataBase");
 
 const productsController = {
-
   products: function (req, res) {
-
     let objet1 = productsList.filter(function (element) {
-      return (element.category == "bicicletas")
-    })
+      return element.category == "bicicletas";
+    });
     let objet2 = productsList.filter(function (element) {
-      return (element.category == "monociclos")
-    })
+      return element.category == "monociclos";
+    });
     let objet3 = productsList.filter(function (element) {
-      return (element.category == "dualciclos")
-    })
+      return element.category == "dualciclos";
+    });
     let objet4 = productsList.filter(function (element) {
-      return (element.category == "monopatines")
-    })
-    let items = [objet1[1], objet2[2],objet3[1], objet4[0]]
+      return element.category == "monopatines";
+    });
+    let items = [objet1[1], objet2[2], objet3[1], objet4[0]];
 
-    res.render("./product/product", { items: items, styleOn: "product" })
+    res.render("./product/product", { items: items, styleOn: "product" });
   },
 
   category: function (req, res) {
-
     let items = productsList.filter(function (valor) {
-      return (valor.category == req.params.category)
-    })
-    res.render("./product/category", { items: items, styleOn: "accessories" })
+      return valor.category == req.params.category;
+    });
+    res.render("./product/category", { items: items, styleOn: "accessories" });
   },
 
   detail: function (req, res) {
     let detalle = productsList.filter(function (valor) {
       if (valor.id == req.params.id) {
-        return valor
+        return valor;
       }
-    })
+    });
 
     let verificar = productsList.filter(function (dato) {
       if (dato.category == "accesorios") {
-        return dato
+        return dato;
       }
-    })
+    });
 
     let detalleAccesorio = verificar.slice(3, 6);
 
     let warranty = guaranteeList.filter(function (valor) {
       if (valor.id == req.params.id) {
-        return valor
+        return valor;
       }
-    })
+    });
 
     let caracteristicas = featuresList.filter(function (valor) {
       if (valor.id == req.params.id) {
-        return valor
+        return valor;
       }
-    })
+    });
 
     res.render("./product/productDetail", {
       detalle: detalle,
-      detalleAccesorio: detalleAccesorio, warranty: warranty,
-      caracteristicas: caracteristicas, convertir: toThousand, styleOn: "productDetail"
-    })
+      detalleAccesorio: detalleAccesorio,
+      warranty: warranty,
+      caracteristicas: caracteristicas,
+      convertir: toThousand,
+      styleOn: "productDetail",
+    });
   },
 
   edit: function (req, res, next) {
-
     let editar = productsList.find(function (buscar) {
       if (buscar.id == req.params.id) {
-        return buscar
+        return buscar;
       }
-    })
+    });
 
     let userFind = usersList.find(function (buscar) {
-      if (buscar.email == res.locals.user ) {
-        return buscar
+      if (buscar.email == res.locals.user) {
+        return buscar;
       }
-    })
+    });
 
-    if(userFind == undefined) {
-        return res.render("user/login", { errors:{}, styleOn: "login" })
-      }
-    else{
-      res.render("./product/editProduct", { editar: editar, styleOn: "create-editProduct" })
+    if (userFind == undefined) {
+      return res.render("user/login", { errors: {}, styleOn: "login" });
+    } else {
+      res.render("./product/editProduct", {
+        editar: editar,
+        styleOn: "create-editProduct",
+      });
     }
   },
 
   update: (req, res) => {
+    let pathFile = path.join("data", "productsDatabase.json");
 
+    let actualProduct = fs.readFileSync(pathFile, { encoding: "utf-8" });
 
-    let pathFile = path.join('data','prueba.json')
+    actualProduct = JSON.parse(actualProduct);
 
-		let actualProduct = fs.readFileSync(pathFile, { encoding: 'utf-8' })
-
-		actualProduct = JSON.parse(actualProduct)
-		
-		actualProduct = actualProduct.map(function(buscar) {
-			
-			if(buscar.id == req.params.id) {
+    actualProduct = actualProduct.map(function (buscar) {
+      if (buscar.id == req.params.id) {
         buscar = {
           ...req.body,
-        image: req.files[0].filename,
+        };
+        buscar.id = req.params.id;
+        console.log(req.files);
+        if (req.files.length == []) {
+          buscar.image = "";
+        } else {
+          buscar.image = req.files[0].filename;
         }
-        buscar.id = req.params.id
-				return buscar
+
+        return buscar;
       }
-		})
-		
-		
-		actualProduct = JSON.stringify(actualProduct)
-	
-		fs.writeFileSync(pathFile, actualProduct)
-		
+    });
 
-		res.redirect('/products')
+    actualProduct = JSON.stringify(actualProduct);
 
+    fs.writeFileSync(pathFile, actualProduct);
 
+    res.redirect("/products");
   },
 
   destroy: (req, res) => {
-    let pathFile = path.join('data', 'productsDatabase.json')
-    let actualProduct = fs.readFileSync(pathFile, { encoding: 'utf-8' })
-    actualProduct = JSON.parse(actualProduct)
+    let pathFile = path.join("data", "productsDatabase.json");
+    let actualProduct = fs.readFileSync(pathFile, { encoding: "utf-8" });
+    actualProduct = JSON.parse(actualProduct);
 
     actualProduct = actualProduct.filter(function (buscar) {
       if (buscar.id != req.params.id) {
-        return buscar
+        return buscar;
       }
-    })
+    });
 
-    actualProduct = JSON.stringify(actualProduct)
+    actualProduct = JSON.stringify(actualProduct);
 
-    fs.writeFileSync(pathFile, actualProduct)
-    res.send('Producto Borrado!!')
+    fs.writeFileSync(pathFile, actualProduct);
+    res.send("Producto Borrado!!");
   },
 
   create: function (req, res, next) {
     let userFind = usersList.find(function (buscar) {
-      if (buscar.email == res.locals.user ) {
-        return buscar
+      if (buscar.email == res.locals.user) {
+        return buscar;
       }
-    })
+    });
 
-    if(userFind == undefined) {
-        return res.render("user/login", { errors:{}, styleOn: "login" })
-      }
-    else{
-      res.render("product/createProduct", { styleOn: "create-editProduct" })
-  }
+    if (userFind == undefined) {
+      return res.render("user/login", { errors: {}, styleOn: "login" });
+    } else {
+      res.render("product/createProduct", { styleOn: "create-editProduct" });
+    }
   },
 
   store: function (req, res, next) {
+    let pathFile = path.join("data", "productsDatabase.json");
 
-    let pathFile = path.join('data', 'productsDatabase.json')
+    let nuevoProduct = fs.readFileSync(pathFile, { encoding: "utf-8" });
 
-    let nuevoProduct = fs.readFileSync(pathFile, { encoding: 'utf-8' })
-
-    nuevoProduct = JSON.parse(nuevoProduct)
+    nuevoProduct = JSON.parse(nuevoProduct);
 
     nuevoProduct.push({
       ...req.body,
       id: nuevoProduct[nuevoProduct.length - 1].id + 1,
-      image: req.files[0].filename
-    })
+      image: req.files[0].filename,
+    });
 
-    nuevoProduct = JSON.stringify(nuevoProduct)
+    nuevoProduct = JSON.stringify(nuevoProduct);
 
-    fs.writeFileSync(pathFile, nuevoProduct)
+    fs.writeFileSync(pathFile, nuevoProduct);
 
-    res.redirect('/products')
+    res.redirect("/products");
   },
 
   cart: function (req, res) {
-
     let items = productsList.filter(function (valor) {
       if (valor.category == "visitados") {
-        return valor
+        return valor;
       }
-    })
+    });
     let esteDato = productsList.filter(function (esteDato) {
       if (esteDato.category == "accesorios") {
-        return esteDato
+        return esteDato;
       }
-    })
+    });
 
     let userFind = usersList.find(function (buscar) {
-      if (buscar.email == res.locals.user ) {
-        return buscar
+      if (buscar.email == res.locals.user) {
+        return buscar;
       }
-    })
+    });
 
-    if(userFind == undefined) {
-        return res.render("user/login", { errors:{}, styleOn: "login" })
-      }
-    else{
-      res.render("./product/productCart", { items: items, esteAccesorio: esteDato, styleOn: "productCart" }) 
+    if (userFind == undefined) {
+      return res.render("user/login", { errors: {}, styleOn: "login" });
+    } else {
+      res.render("./product/productCart", {
+        items: items,
+        esteAccesorio: esteDato,
+        styleOn: "productCart",
+      });
     }
-  }
-}
-
+  },
+};
 
 module.exports = productsController;
