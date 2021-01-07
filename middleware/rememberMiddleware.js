6ab383f1c1a5262c1efaf25c68ac815465fd836c
Gastón Dominguez <gastonmd14@gmail.com>
2021-01-07 19:14:11 -0300

    se actualiza la funcion de recordarme para que use la db

--- middleware/rememberMiddleware.js
+++ middleware/rememberMiddleware.js
@@ -1,10 +1,26 @@
 var modelsUser = require('../models/user')
+const db = require('../database/models');
+const { Op } = require('sequelize')
 
 module.exports = function (req, res, next) {
 
     if(req.session.recordame && !req.session.user) {
-        let user = userData.findByEmail(req.cookie.recordame)
-        req.session.user = user.email
+        db.User.findOne({
+            where: {
+                email: {
+                  [Op.eq]: req.session.user
+                }
+              }
+        })
+        .then((resultado) => {
+            req.session.user = resultado.email
+        })
     }
     next()
+
+    // if(req.session.recordame && !req.session.user) {
+    //     let user = userData.findByEmail(req.cookie.recordame)
+    //     req.session.user = user.email
+    // }
+    // next()
 }
