959cdf0bc904731662dbf94e9bfc5c2fcbec060d
Gastón Dominguez <gastonmd14@gmail.com>
2021-01-16 16:20:39 -0300

    se configura la validacion de backend para email y contraseña

--- middleware/userValidator.js
+++ middleware/userValidator.js
@@ -4,14 +4,21 @@ const db = require('../database/models');
 const { Op } = require('sequelize')
 
 module.exports = [
-  check('email').isEmail().withMessage('El mail debe tener un formato valido'),
-  body("email").custom(function (value) { 
-    return db.User.findOne({
-      where: {
-        email: {
-          [Op.eq]: value
+  check('email')
+    .notEmpty()
+    .withMessage("Campo obligatorio")
+    .bail()
+    .isEmail()
+    .withMessage('El mail debe tener un formato valido')
+    .bail(),
+  body("email")
+    .custom(function (value) { 
+      return db.User.findOne({
+        where: {
+          email: {
+            [Op.eq]: value
+          }
         }
-      }
     })
     .then((resultado) => {
       if (resultado) {
@@ -24,6 +31,12 @@ module.exports = [
     })
   }),
 
-  check('password').isLength({min:8}).withMessage('Su clave debe tener al menos 8 caracteres')
+  check('password')
+    .notEmpty()
+    .withMessage("Campo obligatorio")
+    .bail()
+    .isLength({min:8})
+    .withMessage('Su clave debe tener al menos 8 caracteres')
+    .bail()
 ];
 
