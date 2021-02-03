1f1c0606a2aeefa8388785db5c8432d02f281bbf
Gastón Dominguez <gastonmd14@gmail.com>
2021-02-03 20:18:28 -0300

    se agrega el endpoint con el detalle del usuario

--- controllers/api/usersController.js
+++ controllers/api/usersController.js
@@ -8,10 +8,14 @@ module.exports = {
     list: function(req, res) {
 
 		db.User.findAll({
-			
+            attributes: { exclude: ["username","password","last_name", "address", "profile", "image"] }
+                
+
 		})
 		.then((resultado) => {
-			
+			for(let i = 0; i < resultado.length; i++) {
+				resultado[i].setDataValue('endpoint', '/api/users/' + resultado[i].id)
+			}
 
 			let respuesta = {
 				meta: {
