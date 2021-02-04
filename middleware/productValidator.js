7d824825b96c94fccfcadaa40f28782a2f4c5b94
Gastón Dominguez <gastonmd14@gmail.com>
2021-02-04 20:28:20 -0300

    se arregla el codigo y se quitan los console.log de los validadores

--- middleware/productValidator.js
+++ middleware/productValidator.js
@@ -1,31 +1,30 @@
 const { body, check } = require("express-validator");
-let modelsUsers = require("../models/user");
-const db = require('../database/models');
 
 module.exports = [
-    check('name')
-      .notEmpty()
-      .withMessage("Nombre obligatorio")
-      .isLength({min:5})
-      .withMessage('El nombre debe tener al menos 5 caracteres'),
+  check("name")
+    .notEmpty()
+    .withMessage("Nombre obligatorio")
+    .isLength({ min: 5 })
+    .withMessage("El nombre debe tener al menos 5 caracteres"),
 
-    check('description')
-      .isLength({min:20})
-      .withMessage('Esta descripcion debe tener al menos 20 caracteres'),
+  check("description")
+    .isLength({ min: 20 })
+    .withMessage("Esta descripcion debe tener al menos 20 caracteres"),
 
-    body('image')
-      .custom((value, {req}) => {
-          if(req.files.length == []) {
-              return true
-          } else {
-              let imagen = (req.files[0].originalname).split('.');
-              let allowExt = ['jpg', 'jpeg', 'png', 'gif']
-              let existe = allowExt.includes(imagen[1])
-              if(existe == false) {
-                  throw new Error("La imagen debe ser un archivo valido (JPG, JPEG, PNG, GIF)");
-              } else {
-                  return true;  
-              }
-          }
-        })
-  ];
+  body("image").custom((value, { req }) => {
+    if (req.files.length == []) {
+      return true;
+    } else {
+      let imagen = req.files[0].originalname.split(".");
+      let allowExt = ["jpg", "jpeg", "png", "gif"];
+      let existe = allowExt.includes(imagen[1]);
+      if (existe == false) {
+        throw new Error(
+          "La imagen debe ser un archivo valido (JPG, JPEG, PNG, GIF)"
+        );
+      } else {
+        return true;
+      }
+    }
+  }),
+];
