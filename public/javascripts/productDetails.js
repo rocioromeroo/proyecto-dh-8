8ddae147840967551ae4680d563686cf20eae2b7
Gastón Dominguez <gastonmd14@gmail.com>
2021-02-08 20:33:44 -0300

    se agrega el evento clic en la vista de product detail para informacion, caracteristicas, servicio post venta y garantias

--- public/javascripts/productDetails.js
+++ public/javascripts/productDetails.js
@@ -13,5 +13,70 @@ window.addEventListener("load", function () {
 			display.innerHTML--
 		}
 	})
+
+    function active(el) {
+        el.classList.remove('active')
+        el.classList.add('desactive')
+    }
+
+    function desactive(el) {
+        el.classList.remove('desactive')
+        el.classList.add('active')
+    }
+
+    let infoClic = true
+    let mostrarInfo = document.getElementById('info')
+    mostrarInfo.addEventListener('click', function () {
+        
+        if(infoClic) {
+            active(mostrarInfo.childNodes[3].childNodes[1])
+            infoClic = false
+        } else {
+            desactive(mostrarInfo.childNodes[3].childNodes[1])
+            infoClic = true
+        }
+        
+    })
+
+    let caraClic = true
+    let mostrarCara = document.getElementById('cara')
+    mostrarCara.addEventListener('click', function () {
+        if(caraClic) {
+            desactive(mostrarCara.childNodes[3])
+            caraClic = false
+        } else {
+            active(mostrarCara.childNodes[3])
+            caraClic = true
+        }
+        
+    })
+
+    let servClic = true
+    let mostrarServ = document.getElementById('serv')
+    mostrarServ.addEventListener('click', function () {
+        if(servClic) {
+            desactive(mostrarServ.childNodes[3].childNodes[1])
+            servClic = false
+        } else {
+            active(mostrarServ.childNodes[3].childNodes[1])
+            servClic = true
+
+        }
+        
+    })
+
+    let garaClic = true
+    let mostrarGara = document.getElementById('gara')
+    mostrarGara.addEventListener('click', function () {
+        if(garaClic) {
+            desactive(mostrarGara.childNodes[3])
+            garaClic = false
+        } else {
+            active(mostrarGara.childNodes[3])
+            garaClic = true
+        }
+        
+    })
+    
 })
 
